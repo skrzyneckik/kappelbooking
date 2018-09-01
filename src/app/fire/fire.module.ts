6a8410128f93e75aac212494f180db6ea50e7dc6
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2018-09-01 16:11:46 +0200

    Configured angularfire2

---
+++ src/app/fire/fire.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { firebaseConfig } from './secrets.ts';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
+  ],
+  declarations: []
+})
+export class FireModule { }
