1d275ac4d0a04bd1f0e03e5e42216683bbbb05fd
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2018-09-01 16:14:32 +0200

    Updated name of firebaseconfig

--- src/app/fire/fire.module.ts
+++ src/app/fire/fire.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { firebaseConfig } from './secrets.ts';
+import { firebaseConfig } from './firebase-config.ts';
 
 @NgModule({
   imports: [
